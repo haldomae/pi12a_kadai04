dd46acf120e0842ac4e787555a4f3743e9b79869
haldomae <hal.domae@gmail.com>
2024-06-07 16:54:01 +0900

    クイズをå出題するメソッドを追加

---
+++ app/src/main/java/com/hal_domae/pi12a_kadai04_05/AnswerDialogFragment.kt
@@ -0,0 +1,4 @@
+package com.hal_domae.pi12a_kadai04_05
+
+class AnswerDialogFragment {
+}
--- app/src/main/java/com/hal_domae/pi12a_kadai04_05/MainActivity.kt
+++ app/src/main/java/com/hal_domae/pi12a_kadai04_05/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.hal_domae.pi12a_kadai04_05
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,6 +10,16 @@ import com.hal_domae.pi12a_kadai04_05.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    // クイズの正解
+    private var rightAnswer: String? = null
+    // 何問目か
+    private var quizCount = 1
+    private val quizData = mutableListOf(
+        listOf("パンはパンでも食べられないパンは", "フライパン"),
+        listOf("パンはパンでも食べられないパンは", "フライパン"),
+        listOf("パンはパンでも食べられないパンは", "フライパン"),
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,5 +30,17 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        showNextQuiz()
+
+    }
+
+    // クイズを出題するメソッド
+    private fun showNextQuiz(){
+        // 問題番号を表示
+        binding.countLabel.text = getString(R.string.count_label, quizCount)
+
+        // クイズの問題をセット
+        val quiz = quizData[0]
+        binding.questionLabel.text = quiz[0]
     }
 }
